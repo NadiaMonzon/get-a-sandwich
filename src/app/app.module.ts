c3a66966d63fa2806f0d50adbf4ee3768af55daf
saramonzon22 <sara.mon.quesada@gmail.com>
2023-06-16 20:32:17 +0100

    navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './pages/login/login.component';
+import { SearchComponent } from './pages/search/search.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
+    SearchComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
