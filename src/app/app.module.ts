dd15d29d30022a7099e0a8f43e07d1cb6c82473f
SaraMonzon <sara.mon.quesada@gmail.com>
2023-08-21 18:48:57 +0100

    feat(get-a-sandwich): add initial router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './pages/login/login.component';
-import { SearchComponent } from './pages/search/search.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { SandwichDetailComponent } from './pages/sandwich-detail/sandwich-detail.component';
+
+
+const ROUTES: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'home', loadChildren: () => import('../app/pages/home/home.module').then(m => m.HomeModule)},
+
+  {path: '**', redirectTo: 'home'}
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    LoginComponent,
-    SearchComponent,
-    NavbarComponent,
-    SandwichDetailComponent,
+    AppComponent, 
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule.forRoot(ROUTES),
     BrowserAnimationsModule
   ],
   providers: [],
